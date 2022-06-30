954c44ff0a098f6e7ad35d52f379b28651cafe20
DESKTOP-Q8UF7RN\Henrique <henriquecristiandasilva@gmail.com>
2022-06-30 20:35:22 -0300

    Ajuste testes unitários

--- backend/cardapioDigital/src/test/java/br/com/digitaLife/cardapioDigital/controller/EmpresaControllerTest.java
+++ backend/cardapioDigital/src/test/java/br/com/digitaLife/cardapioDigital/controller/EmpresaControllerTest.java
@@ -1,10 +1,18 @@
 package br.com.digitaLife.cardapioDigital.controller;
 
+import br.com.digitaLife.cardapioDigital.dto.EmpresaDto;
 import br.com.digitaLife.cardapioDigital.service.EmpresaService;
+import br.com.digitaLife.cardapioDigital.util.EmpresaCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,7 +25,13 @@ class EmpresaControllerTest {
     @Mock
     private EmpresaService empresaService;
 
-
+    @Test
+    @DisplayName("Salva um empresa quando sucesso")
+    void salve_retorna_pessoa_quando_sucesso() throws IOException {
+        EmpresaDto empresaDto = EmpresaCreator.createEmpresaForSave();
+        ResponseEntity<Object> responseEntity = empresaController.saveEmpresa(empresaDto);
+        Assertions.assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
+    }
 
 
 }
