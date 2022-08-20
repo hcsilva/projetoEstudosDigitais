7a2866d483142251740af35f4574bd4284e40377
Henrique <henrique.silva@evoluum.com.br>
2022-08-19 21:06:01 -0300

    Criação dos profiles de autenticação. Criado teste unitário de empresa

---
+++ backend/cardapioDigital/src/main/java/br/com/digitaLife/cardapioDigital/config/security/DevWebSecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.digitaLife.cardapioDigital.config.security;
+
+import br.com.digitaLife.cardapioDigital.enums.RoleNameEnum;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigDEV {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/api/empresa/**").hasRole(RoleNameEnum.ADMIN.name())
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
